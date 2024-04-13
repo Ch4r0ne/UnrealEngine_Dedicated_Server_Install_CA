8ac7886c08260f9bd7112ac3f1126fdaed3e470e
Ch4r0ne <54702161+Ch4r0ne@users.noreply.github.com>
2024-04-13 09:32:51 +0200

    Update Install_Certificate.ps1

--- Install_Certificate.ps1
+++ Install_Certificate.ps1
@@ -18,30 +18,21 @@ $targetDirectory = "$env:TEMP\r2m02.cer"
 
 try {
     # Download the Amazon Root CA and the certificate
-    Invoke-WebRequest -Uri $amazonRootCAUrl -OutFile $amazonRootCADirectory -UseBasicParsing -ErrorAction Stop
-    Invoke-WebRequest -Uri $certificateUrl -OutFile $targetDirectory -UseBasicParsing -ErrorAction Stop
+    Invoke-RestMethod -Uri $amazonRootCAUrl -OutFile $amazonRootCADirectory -ErrorAction Stop
+    Invoke-RestMethod -Uri $certificateUrl -OutFile $targetDirectory -ErrorAction Stop
 
     # Import Amazon Root CA into the CA store of the current user
     Import-Certificate -FilePath $amazonRootCADirectory -CertStoreLocation Cert:\CurrentUser\CA -ErrorAction Stop
-    Write-Host "Amazon Root CA has been installed in the CA store of the current user."
+    Write-Output "Amazon Root CA has been installed in the CA store of the current user."
 
     # Import the certificate into the personal store of the current user
-    $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-    $cert.Import($targetDirectory)
-
-    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("CA", "CurrentUser")
-    $store.Open("ReadWrite")
-    $store.Add($cert)
-    $store.Close()
-    Write-Host "The certificate has been installed in the personal store of the current user."
+    Import-Certificate -FilePath $targetDirectory -CertStoreLocation Cert:\CurrentUser\My -ErrorAction Stop
+    Write-Output "The certificate has been installed in the personal store of the current user."
 
     # Import the certificate into the machine store (LocalMachine)
-    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("CA", "LocalMachine")
-    $store.Open("ReadWrite")
-    $store.Add($cert)
-    $store.Close()
-    Write-Host "The certificate has been installed in the machine store (LocalMachine)."
+    Import-Certificate -FilePath $targetDirectory -CertStoreLocation Cert:\LocalMachine\CA -ErrorAction Stop
+    Write-Output "The certificate has been installed in the machine store (LocalMachine)."
 }
 catch {
-    Write-Error "An error occurred while installing the certificate: $_"
+    throw "An error occurred while installing the certificate: $_"
 }
